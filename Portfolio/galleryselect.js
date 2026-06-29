f898647425864147122b43d56e2796ef79b845f5
shyinka <sara.charouzova@seznam.cz>
2026-06-29 10:15:39 +0200

    Delete Portfolio/galleryselect.js

--- Portfolio/galleryselect.js
+++
@@ -1,8 +0,0 @@
-const link = document.createElement("a");
-link.href = item.url;
-
-const img = document.createElement("img");
-img.src = item.image;
-
-link.appendChild(img);
-gallery.appendChild(link);
